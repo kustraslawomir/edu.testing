454c733e6769088bcbb27521b7d646661d3aa80e
s.kustra <skustra@mobitouch.net>
2022-11-24 08:53:30 +0100

    dependencies

--- app/src/test/java/edu/testing/ExampleUnitTest.kt
+++ app/src/test/java/edu/testing/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package edu.testing
 
-import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +13,10 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun addition_isInCorrect() {
+        assertEquals(5, 2 + 2)
     }
 }
